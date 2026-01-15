ce9d2228137cca5eff22e910e6bf3a8004926b14
Areeba Abid <aareeba006@gmail.com>
2026-01-15 21:23:47 +0400

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,3 +1,4 @@
+#this is a solution
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)-1): # we write -1 to tell ourselves that two pointer approach is used
