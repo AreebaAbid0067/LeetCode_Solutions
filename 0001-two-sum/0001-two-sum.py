49e82b8e1e9b73a20fc3f17fac68b3d059e71177
Areeba Abid <aareeba006@gmail.com>
2026-02-24 23:25:18 +0500

    Time: 1973 ms (7.45%), Space: 19.8 MB (77.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,17 @@
-#this is a solution
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)-1): # we write -1 to tell ourselves that two pointer approach is used
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j]==target:
-                    return(i,j)
+        result = []
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    result.append(i)
+                    result.append(j)
+
+        return result
+
+                
+
+
+        
+
+            
