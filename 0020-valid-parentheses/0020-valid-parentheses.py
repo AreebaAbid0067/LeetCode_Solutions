bc13ffd41d4a3fb8700134aa93c54b81bece5fc5
Areeba Abid <aareeba006@gmail.com>
2026-03-21 21:51:13 +0500

    Time: 0 ms (100%), Space: 19.5 MB (21.46%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,24 +1,9 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # approach 1
-        # stack = []
-        # openToCLose = { "}": "{", "]": "[" ,")":"("}
-
-        # for c in s:
-        #     if c in openToCLose:
-        #         if  stack and stack[-1] == openToCLose[c]:  #stack [-1] means accesssing top element
-        #             stack.pop()
-        #         else:
-        #             return False
-        #     else:
-        #         stack.append(c)
-
-        # return True if not stack else False
-
-        # approach 2
         stack = []
-        start = "([{"
-        end = ")]}"
+        start = "{[("
+        end = "}])"
+
         for i in s:
             if i in start:
                 stack.append(i)
@@ -30,9 +15,5 @@ class Solution:
                 else:
                     return False
 
+
         return len(stack) == 0
-
-
-        
-
-
